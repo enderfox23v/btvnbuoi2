a3c34e3117f732631c4726a256c3b6b2f4206933
enderfox23v <enderfox23v@gmail.com>
2026-07-21 22:48:57 +0700

    hehe

--- bai2,4.py
+++
@@ -1,7 +0,0 @@
-x= int (input ('nhap so tien ban co: '))
-t= x//28
-if t>=3 :
-    doi = t//3
-    print ("du ", t, "vo doi them duoc", doi, " chai nua ")
-    t=t+doi
-print ("mua duoc ", t, "chai")
